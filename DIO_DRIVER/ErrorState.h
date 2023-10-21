e777219a08721671eab9523f8ea3cc5363675089
wesam98 <120117841+wesam98@users.noreply.github.com>
2023-10-21 21:02:33 +0300

    initial commit

---
+++ DIO_DRIVER/ErrorState.h
@@ -0,0 +1,22 @@
+/*
+ * ErrorState.h
+ *
+ *  Created on: Oct 13, 2023
+ *      Author: METRA
+ */
+
+#ifndef ERRORSTATE_H_
+#define ERRORSTATE_H_
+
+
+#ifndef ERRORSTATES_H_
+#define ERRORSTATES_H_
+
+enum { ES_OK,
+	   ES_NOK,
+	   ES_NULL_POINTER,
+	   ES_OUT_OF_RANGE
+}ES;
+
+
+#endif /* ERRORSTATE_H_ */
