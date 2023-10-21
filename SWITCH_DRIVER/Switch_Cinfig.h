f40ed9bd52a4e92f182158954eaf9adf5aab5f88
wesam98 <120117841+wesam98@users.noreply.github.com>
2023-10-21 21:19:50 +0300

    Initial Commit

---
+++ SWITCH_DRIVER/Switch_Cinfig.h
@@ -0,0 +1,22 @@
+/*
+ * Switch_Cinfig.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: METRA
+ */
+#include "StdTypes.h"
+#ifndef SWITCH_CINFIG_H_
+#define SWITCH_CINFIG_H_
+
+#define SW_NUM    3
+
+typedef struct
+{
+  u8 SW_PORT_ID;
+  u8 SW_PIN_ID;
+  u8 SW_STATUS;
+
+}SW_t;
+
+
+#endif /* SWITCH_CINFIG_H_ */
---
+++ SWITCH_DRIVER/main.c
@@ -0,0 +1,14 @@
+/*
+.0n * main.c
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: METRA
+ */
+#include "Switch_Cinfig.h"
+
+extern SW_t SWITCH_AstrSWStatus [SW_NUM];
+int main (void)
+{
+
+return 0;
+}
