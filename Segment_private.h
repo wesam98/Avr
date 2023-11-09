65392215ebb95a52c7f049286a31dca3ff2a69e0
wesam98 <120117841+wesam98@users.noreply.github.com>
2023-11-09 05:46:07 +0200

    Initial commit

---
+++ Segment_private.h
@@ -0,0 +1,23 @@
+/*
+ * Segment_private.h
+ *
+ *  Created on: Nov 7, 2023
+ *      Author: METRA
+ */
+
+#ifndef SEGMENT_PRIVATE_H_
+#define SEGMENT_PRIVATE_H_
+
+#define COMMON_ANODE   21
+#define COMMON_CATHODE 20
+
+#define NOT_CONNECTED  10
+#define CONNECTED      12
+
+u8 SEG_Au8Display[]={  0x3f, 0x06, 0x5B,
+		0x4F, 0x66, 0x6D,
+		0x7D, 0x07, 0x7F,
+		0x6F};
+
+
+#endif /* SEGMENT_PRIVATE_H_ */
