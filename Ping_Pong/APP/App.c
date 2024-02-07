e8b6cec3369dc6eaea85879f3fbe16b369da9e96
Wesam Abdelaziz <120117841+wesam98@users.noreply.github.com>
2024-02-08 01:55:39 +0200

    initial commot

---
+++ Ping_Pong/APP/App.c
@@ -0,0 +1,105 @@
+/* * App.c
+ *
+ *  Created on: Feb 6, 2024
+ *      Author: METRA
+ */
+
+#include "../MCAL/DIO_Int.h"
+#include "../HAL/LCD/LCD_Int.h"
+#include "App.h"
+#include "util/delay.h"
+u8 win1 = 0,win2 = 0,score1 = 0,score2 = 0;
+u8 row_ball=2,col_ball=2;
+
+void Ping_PongInit()
+{
+	LCD_GOTO(0,1);
+	LCD_enuWriteString("p1 ");
+	LCD_enuDisplayIntegerNum(score1);
+	LCD_GOTO(0,10);
+	LCD_enuWriteString("p2 ");
+	LCD_enuDisplayIntegerNum(score2);
+	LCD_GOTO(2,15);
+	LCD_enuDisplayChar('|');
+
+	LCD_GOTO(2,0);
+	LCD_enuDisplayChar('|');
+
+	LCD_GOTO(2,1);
+	LCD_enuDisplayChar('O');
+	_delay_ms(50);
+	LCD_GOTO(2,1);
+	LCD_enuDisplayChar(' ');
+
+
+}
+
+
+void Ping_PongStart()
+{
+
+	for( ;col_ball<14;col_ball++){
+		row_ball=((col_ball<<0)&1)+2;
+
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar('O');
+		_delay_ms(50);
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar(' ');
+	}
+	for( ;col_ball>1;col_ball--){
+		row_ball=((col_ball<<0)&1)+2;
+
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar('O');
+		_delay_ms(50);
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar(' ');
+	}
+}
+
+
+void ISR_INT0()
+{
+	for( ;col_ball<14;col_ball++){
+		row_ball=((col_ball<<0)&1)+2;
+
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar('O');
+		_delay_ms(50);
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar(' ');
+	}
+	LCD_ClearDisp();
+	LCD_GOTO(1,1);
+	LCD_enuWriteString("Player 1 Won...");
+	score1++;
+	_delay_ms(500);
+	LCD_ClearDisp();
+	Ping_PongInit();
+	row_ball=2;
+	col_ball =2;
+
+}
+void ISR_INT1()
+{
+
+	for(;col_ball>1;col_ball--){
+		row_ball=((col_ball<<0)&1)+2;
+
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar('O');
+		_delay_ms(50);
+		LCD_GOTO(row_ball,col_ball);
+		LCD_enuDisplayChar(' ');
+	}
+	LCD_ClearDisp();
+	LCD_GOTO(1,1);
+	LCD_enuWriteString("Player 2 Won...");
+	score2++;
+	_delay_ms(500);
+	LCD_ClearDisp();
+	Ping_PongInit();
+	row_ball=3;
+	col_ball =2;
+}
---
+++ Ping_Pong/APP/App.h
@@ -0,0 +1,18 @@
+/*
+ * App.h
+ *
+ *  Created on: Feb 6, 2024
+ *      Author: METRA
+ */
+
+#ifndef APP_APP_H_
+#define APP_APP_H_
+
+
+void Ping_PongInit();
+void Ping_PongStart();
+void ISR_INT0();
+void ISR_INT1();
+
+
+#endif /* APP_APP_H_ */
